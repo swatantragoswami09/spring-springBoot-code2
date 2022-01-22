d04f004a1afccf6b20ef4ea0f3ffb9d81411616f
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2022-01-22 22:41:08 +0530

    @Value annotation

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -3,9 +3,11 @@ package com.luv2code.springdemo;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @Configurable
 // @ComponentScan("com.luv2code.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 
     // define bean for our sad fortune service
