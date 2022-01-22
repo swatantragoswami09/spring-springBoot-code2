903d19e0536006cf45314e2687406762a5443486
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2022-01-22 20:34:48 +0530

    @Qualifier annotation illustration

---
+++ src/com/luv2code/springdemo/RandomService.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo;
+
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class RandomService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+    
+}
