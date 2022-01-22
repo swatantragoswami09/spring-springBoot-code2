b00067d7baf4848242213af2a146b5bbf63353b9
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2022-01-22 22:17:34 +0530

    read spring config java classs

---
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configurable
+@ComponentScan("com.luv2code.springdemo")
+public class SportConfig {
+    
+}
--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -1,12 +1,13 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach{
 
     @Autowired
@@ -18,18 +19,17 @@ public class TennisCoach implements Coach{
         System.out.println(">> TennisCoach: inside default constructor");
     }
     
-    // define a setter method
-    // @Autowired
-    // public void doSomeCrazyStuff(FortuneService thefortuneService){
-    //     System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
-    //     fortuneService=thefortuneService;
-    // }
-
-    // @Autowired
-    // public TennisCoach(FortuneService fortuneService) {
-    //     this.fortuneService = fortuneService;
-    // }
-
+    //    define my init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">>TennisCoach: inside of doMyStartupStuff()");
+        
+    }
+    //  define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennishCoach: inside of doMyCleanupStuff()");
+    }
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
