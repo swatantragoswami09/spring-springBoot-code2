ab3e698ce6b4ab18b7a29223bc68fc1282d80117
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2022-01-22 20:17:22 +0530

    @Autowired with setter methods

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -8,15 +8,28 @@ public class TennisCoach implements Coach{
     
     private FortuneService fortuneService;
 
-    @Autowired
-    public TennisCoach(FortuneService fortuneService) {
-        this.fortuneService = fortuneService;
+    // defien a default constructor
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
     }
+    
+    // define a setter method
+    @Autowired
+    public void doSomeCrazyStuff(FortuneService thefortuneService){
+        System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+        fortuneService=thefortuneService;
+    }
+
+    // @Autowired
+    // public TennisCoach(FortuneService fortuneService) {
+    //     this.fortuneService = fortuneService;
+    // }
 
     @Override
     public String getDailyWorkout() {
         return "Practice your backhand volley";
     }
+
 
     @Override
     public String getDailyFortune() {
